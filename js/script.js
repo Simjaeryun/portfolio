124075a7d3c377d4a54505aa828d4b282e48142c
Simjaeryun <61261788+Simjaeryun@users.noreply.github.com>
2020-07-16 18:55:04 +0900

    0716
    0716

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function () {
+  $("#simjaeryun").click(function () {
+    $(".simjaeryun_popup").show();
+  });
+  $(".popup_close").click(function () {
+    $(".simjaeryun_popup").hide();
+  });
+});
+
+$(function () {
+  $("#skill").click(function () {
+    $(".skill_popup").show();
+  });
+  $(".popup_close").click(function () {
+    $(".skill_popup").hide();
+  });
+});
+
+$(function () {
+  $("#information").click(function () {
+    $(".information_popup").show();
+  });
+  $(".popup_close").click(function () {
+    $(".information_popup").hide();
+  });
+});
+
+$(function () {
+  $("#memo").click(function () {
+    $(".memo_popup").show();
+  });
+  $(".popup_close").click(function () {
+    $(".memo_popup").hide();
+  });
+});
